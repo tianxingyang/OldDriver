07400ed62aa337a5ea9b6df419cce94f1e254bcf
杨天幸 <versacrop@outlook.com>
2017-04-24 11:44:56 +0800

    数组初始化相关

--- misc/miscMain.cpp
+++ misc/miscMain.cpp
@@ -1,9 +1,11 @@
 #include "misc.h"
 #include <iostream>
+#include <cstring>
+using namespace std;
 
 int main()
 {
-    DerivedClass *derivedClassAlpha = new DerivedClass;
+    /*DerivedClass *derivedClassAlpha = new DerivedClass;
     derivedClassAlpha->printme();
     delete derivedClassAlpha;
 
@@ -12,10 +14,15 @@ int main()
     if ((num & 1) == 0)
     {
         std::cout << "num is even" << std::endl;
-    }
+    }*/
+    int initArray[3] = { 2, 4, 6 };
+    int iTest[3] = { 1, 3, 5 };
+    PrintMe(iTest, 3);
+    memcpy(iTest, initArray, sizeof(iTest));
+    PrintMe(iTest, 3);
 
     //退出前有个输入，便于检查程序运行情况
-    bool closeFlag = false;
+    char closeFlag = 'f';
     std::cin >> closeFlag;
     return 0;
 }
