342ffba5402e1f9c2e3183e792ea4f8416bed775
杨天幸 <versacrop@outlook.com>
2018-01-09 10:50:21 +0800

    修改my_binary_tree_node

--- binary_tree/my_binary_tree_node.h
+++ binary_tree/my_binary_tree_node.h
@@ -1,13 +1,21 @@
 #ifndef MY_BINARY_TREE_NODE_H_
 #define MY_BINARY_TREE_NODE_H_
 
+enum ChildId
+{
+    kLeftChild = 0,
+    kRightChild = 1,
+};
+
 template <class T>
 class MyBinaryTreeNode
 {
 public:
-    MyBinaryTreeNode<T>() {}
+    MyBinaryTreeNode():left_(nullptr), right_(nullptr), parent_(nullptr) {}
+    MyBinaryTreeNode(T data, MyBinaryTreeNode<T>* left_child = nullptr, MyBinaryTreeNode<T>* right_child = nullptr, MyBinaryTreeNode<T>* parent = nullptr): 
+        left_(left_child), right_(right_child), parent_(parent) {}
     //virtual MyBinaryTreeNode<T>& operator= (const T& rv) = 0;
-    ~MyBinaryTreeNode<T>() {}
+    ~MyBinaryTreeNode() {}
 
 public:
     bool IsLeftNull() { return left_ == nullptr; }
@@ -15,16 +23,19 @@ public:
 
     MyBinaryTreeNode* GetLeftChild() const { return left_; }
     MyBinaryTreeNode* GetRightChild() const { return right_; }
+    MyBinaryTreeNode* GetParent() const { return parent_; }
     void SetLeftChild(MyBinaryTreeNode<T>* left) { left_ = left; }
     void SetRightChild(MyBinaryTreeNode<T>* right) { right_ = right; }
+    void SetParent(MyBinaryTreeNode<T>* parent) { parent_ = parent; }
 
     T GetData() const { return data_; }
     void SetData(const T& data);
 
 private:
     T data_;
-    MyBinaryTreeNode* left_ = nullptr;
-    MyBinaryTreeNode* right_ = nullptr;
+    MyBinaryTreeNode* left_;
+    MyBinaryTreeNode* right_;
+    MyBinaryTreeNode* parent_;
 };
 
 template<class T>
